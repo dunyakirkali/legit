0cf911d234451f47e6b2b9a91f0d830317f7af90
Dunya Kirkali <dunyakirkali@gmail.com>
2020-03-21 13:59:42 +0100

    Final commit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,9 @@ require 'linguist'
 
 class DashboardController < ApplicationController
   def index
-    repo = Rugged::Repository.new('tmp/repos/loyo')
-    @project = Linguist::Repository.new(repo, repo.head.target_id)
+    platform = params["platform"] || "ios"
+    repo = Rugged::Repository.new("tmp/repos/mobiel-bankieren-#{platform}")
+    @languages = Linguist::Repository.new(repo, repo.head.target_id).languages.to_h
+    @files = repo.index.map { |file| [File.basename(file[:path]), Random.rand(1000)] }.to_h#.sort_by {|k,v| v}.reverse
   end
 end
